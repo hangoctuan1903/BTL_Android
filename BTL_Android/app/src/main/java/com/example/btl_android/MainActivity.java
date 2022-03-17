5cba7750766a5782fabadf4265e87a277fa4797b
huyha1501 <87639541+huyha1501@users.noreply.github.com>
2022-03-17 23:15:24 +0700

    17/3 lúc 11h15

--- BTL_Android/app/src/main/java/com/example/btl_android/MainActivity.java
+++ BTL_Android/app/src/main/java/com/example/btl_android/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.btl_android;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +17,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment = new Home();
+                    break;
+                case R.id.quanly:
+                    selectedFragment = new Quanly();
+                    break;
+                case R.id.poss:
+                    selectedFragment = new dangtin();
+                    break;
+                case R.id.thongbao:
+                    selectedFragment = new thongbao();
+                    break;
+                case R.id.add:
+                    selectedFragment = new them();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
+        }
+    };
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, (Menu) item);
-
-        return super.onOptionsItemSelected(item);
-    }
-}
